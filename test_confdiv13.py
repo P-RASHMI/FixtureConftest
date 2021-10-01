648a069c0f9d5dcfff1b61895d4b1b40b129e5e8
P-RASHMI <imrisurashmi25@gmail.com>
2021-10-01 09:47:32 +0530

    [Rashmi] Add: program has test cases uses source from confest

---
+++ test_confdiv13.py
@@ -0,0 +1,14 @@
+'''
+@Author: Rashmi
+@Date: 2021-09-30 20:17
+@Last Modified by: Rashmi
+@Last Modified time: 2021-09-30 20:20
+@Title :Example to perform conftest concept to extension to use resource for 
+multiple files
+'''
+import pytest
+def test_div_39(input_value):
+    '''
+    Description: this takes value from fixture
+    '''
+    assert input_value % 39 == 0
